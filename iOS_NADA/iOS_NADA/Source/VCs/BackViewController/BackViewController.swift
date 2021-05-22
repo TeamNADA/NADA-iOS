37bb164873990427f37e834fa37d68336eba73e6
hyungyu Kim <ypsophila97@icloud.com>
2021-05-23 04:01:13 +0900

    [FEAT] FrontVC, BackVC 구현 완성. 더미데이터 사용.

--- iOS_NADA/iOS_NADA/Source/VCs/BackViewController/BackViewController.swift
+++ iOS_NADA/iOS_NADA/Source/VCs/BackViewController/BackViewController.swift
@@ -8,26 +8,90 @@
 import UIKit
 
 class BackViewController: UIViewController {
+    //MARK: - Properties
+    private var enrollList = EnrollReqest(id: "1", email: "email", isOB: true, part: "28기 YB iOS", age: "25", insta: "@insta_1", school: "seouluniversity", name: "hyungyu", station: "공릉역", keyword: "#안녕하세요", detail: Detail(favBaskin: "민초", favFood: "피자", nickname: "철수", msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."), essential: Essential(mbti: "esfj", isMincho: true, isBumuk: false, isSoju: false))
+    
 
+    //MARK: - @IBOutlet Properties
     @IBOutlet weak var touchBackBtn: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var keywordLabel: UILabel!
+    @IBOutlet weak var mbtiLabel: UILabel!
+    @IBOutlet weak var mbtiAnswerLabel: UILabel!
+    @IBOutlet weak var isBumukLabel: UILabel!
+    @IBOutlet weak var isBumukAnswerLabel: UILabel!
+    @IBOutlet weak var favBeskinLabel: UILabel!
+    @IBOutlet weak var favBeskinAnswerLabel: UILabel!
+    @IBOutlet weak var favFoodLabel: UILabel!
+    @IBOutlet weak var favFoodAnswerLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var nicknameAnswerLabel: UILabel!
+    @IBOutlet weak var msgLabel: UILabel!
+    @IBOutlet weak var msgAnswerLabel: UILabel!
+    @IBOutlet weak var isMinchoLabel: UILabel!
+    @IBOutlet weak var isMinchoAnswerLabel: UILabel!
+    @IBOutlet weak var isSojuLabel: UILabel!
+    @IBOutlet weak var isSojuAnswerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func backToFront(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+extension BackViewController {
+    private func setUI(){
+//        enrollList =
+//            EnrollReqest(id: "1", email: "email", isOB: true, part: "28기 YB iOS", age: "25", insta: "@insta_1", school: "seouluniversity", name: "hyungyu", station: "공릉역", keyword: "#안녕하세요", detail: Detail(favBaskin: "민초", favFood: "피자", nickname: "철수", msg: "감사합니다"), essential: Essential(mbti: "esfj", isMincho: true, isBumuk: false, isSoju: false))
+        
+        nameLabel.text = enrollList.name
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        keywordLabel.text = enrollList.keyword
+        keywordLabel.font = UIFont.systemFont(ofSize: 16)
+        
+        mbtiLabel.text = "👀 MBTI"
+        mbtiAnswerLabel.text = enrollList.essential.mbti
+        
+        isBumukLabel.text = "👇 부먹/찍먹"
+        if enrollList.essential.isBumuk == true {
+            isBumukAnswerLabel.text = "부먹!"
+        } else {
+            isBumukAnswerLabel.text = "찍먹!"
+        }
+        
+        favBeskinAnswerLabel.text = "🍨 베라 최애 메뉴"
+        favBeskinAnswerLabel.text = enrollList.detail.favBaskin
+        
+        favFoodLabel.text = "🍽 최애 음식"
+        favFoodAnswerLabel.text = enrollList.detail.favFood
+        
+        nicknameLabel.text = "🍽 별명"
+        nicknameAnswerLabel.text = enrollList.detail.nickname
+        
+        msgLabel.text = "🍽 하고싶은말"
+        msgAnswerLabel.text = enrollList.detail.msg
+        
+        isMinchoLabel.text = "🍵 민트초코"
+        if enrollList.essential.isMincho == true {
+            isMinchoAnswerLabel.text = "민초!"
+        } else {
+            isMinchoAnswerLabel.text = "반민초!"
+        }
+        
+        isSojuLabel.text = "🥃 소주/맥주"
+        if enrollList.essential.isSoju == true {
+            isSojuAnswerLabel.text = "소주"
+        } else {
+            isSojuAnswerLabel.text = "맥주"
+        }
     }
-    */
-
 }
--- iOS_NADA/iOS_NADA/Source/VCs/FrontViewController/FrontViewController.swift
+++ iOS_NADA/iOS_NADA/Source/VCs/FrontViewController/FrontViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 class FrontViewController: UIViewController {
 
     //MARK: - Properties
-    
+    private var enrollList = EnrollReqest(id: "1", email: "1234@email", isOB: true, part: "28기 YB iOS", age: "25", insta: "@insta_1", school: "seouluniversity", name: "hyungyu", station: "공릉역", keyword: "#안녕하세요", detail: Detail(favBaskin: "민초", favFood: "피자", nickname: "철수", msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."), essential: Essential(mbti: "esfj", isMincho: true, isBumuk: false, isSoju: false))
     
     //MARK: - @IBOutlet Properties
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var keywordLabel: UILabel!
+    @IBOutlet weak var gradeQlabel: UILabel!
     @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var instaQlabel: UILabel!
     @IBOutlet weak var instagramLabel: UILabel!
+    @IBOutlet weak var universityQlabel: UILabel!
     @IBOutlet weak var universityLabel: UILabel!
+    @IBOutlet weak var emailQlabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var subwayQlabel: UILabel!
     @IBOutlet weak var subwayLabel: UILabel!
     @IBOutlet weak var backUiview: UIView!
+    @IBOutlet weak var shadowView: UIView!
     
     //MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -31,10 +37,7 @@ class FrontViewController: UIViewController {
 
     
     @IBAction func goToBack(_ sender: Any) {
-        let nextVC = BackViewController()
-        
-        self.present(nextVC, animated: true, completion: nil)
-//        self.navigationController?.pushViewController(nextVC, animated: true)
+ 
     }
     
 
@@ -42,15 +45,36 @@ class FrontViewController: UIViewController {
 
 extension FrontViewController {
     private func setUI() {
-        backUiview.layer.cornerRadius = 20
+        shadowView.layer.shadowOpacity = 0.1
+        shadowView.layer.shadowRadius = 5
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        shadowView.layer.shadowColor =  UIColor.black.cgColor
+        shadowView.layer.masksToBounds = false
         
-        nameLabel.text = "이름"
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        keywordLabel.text = "키워드"
-        keywordLabel.font = UIFont.systemFont(ofSize: 14)
-        gradeLabel.text = "28기 YB iOS"
-        instagramLabel.text = "@인스타"
-        universityLabel.text = "서울대학교"
-        subwayLabel.text = "남구로역"
+        gradeQlabel.text = "활동 기수"
+        gradeQlabel.font = UIFont.systemFont(ofSize: 16)
+        instaQlabel.text = "인스타그램 아이디"
+        instaQlabel.font = UIFont.systemFont(ofSize: 16)
+        universityQlabel.text = "학교"
+        universityQlabel.font = UIFont.systemFont(ofSize: 16)
+        emailQlabel.text = "이메일"
+        emailQlabel.font = UIFont.systemFont(ofSize: 16)
+        subwayQlabel.text = "지하철역"
+        subwayQlabel.font = UIFont.systemFont(ofSize: 16)
+        
+        nameLabel.text = enrollList.name
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        keywordLabel.text = enrollList.keyword
+        keywordLabel.font = UIFont.systemFont(ofSize: 16)
+        gradeLabel.text = enrollList.part
+        gradeLabel.font = UIFont.systemFont(ofSize: 20)
+        instagramLabel.text = enrollList.insta
+        instagramLabel.font = UIFont.systemFont(ofSize: 20)
+        universityLabel.text = enrollList.school
+        universityLabel.font = UIFont.systemFont(ofSize: 20)
+        emailLabel.text = enrollList.email
+        emailLabel.font = UIFont.systemFont(ofSize: 20)
+        subwayLabel.text = enrollList.station
+        subwayLabel.font = UIFont.systemFont(ofSize: 20)
     }
 }
